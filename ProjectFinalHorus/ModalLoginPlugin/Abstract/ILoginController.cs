a8807a67a31b96c98b12d4c73be78fa4dbee16fc
Alejanky <alejandr_venegas@hotmail.com>
2018-06-10 18:09:36 -0700

    Terminos y otros cambios en nuget

---
+++ ProjectFinalHorus/ModalLoginPlugin/Abstract/ILoginController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFinalHorus.ModalLogin.Abstract
+{
+    internal interface ILoginController<TUserModel, TResetPasswordViewModel>
+    {
+        JsonResult SignIn(string login_username, string login_password, bool login_rememberme);
+        JsonResult LostPassword(string lost_email);
+        ActionResult SignOut();
+
+        ActionResult UserProfile();
+        ActionResult EditProfile();
+        ActionResult EditProfile(TUserModel user, HttpPostedFileBase ProfileImage);
+        ActionResult DeleteProfile();
+        ActionResult ResetPassword(Guid? id);
+        ActionResult ResetPassword(Guid? id, TResetPasswordViewModel model);
+    }
+}
